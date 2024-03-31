e778ff515e81021e63df424e29ce0c81e622572b
Prakash Yogi <sumrender.singh@codinova.com>
2024-03-31 22:02:32 +0530

    changed script a little bit to get realtime results

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-################################################################
-# Install:
-# * python.exe -m pip install pypiwin32
-# * python.exe -m pip install wmi
-#
-# Usage:
-# * !! Make sure librehardwaremonitor is running !!
-# * python basicwmi.py
-################################################################
-
 import wmi
 import csv
 import time
 
 from datetime import datetime
 
-hwmon = wmi.WMI(namespace="root\LibreHardwareMonitor")
-sensors = hwmon.Sensor(SensorType="Temperature")
-
-# File name format is dataset_YYYY-MM-DD.csv
-with open(f"dataset_{datetime.now().date()}.csv", "w", newline="") as file:
+# File name format is dataset_posixtimestamp.csv
+with open(f"dataset_{datetime.now().timestamp()}.csv", "w", newline="") as file:
     writer = csv.writer(file)
+    
+    hwmon = wmi.WMI(namespace="root\LibreHardwareMonitor")
+    sensors = hwmon.Sensor(SensorType="Temperature")
 
-    writer.writerow(['Timestamp'] + [s.Name for s in sensors])
+    writer.writerow(['Timestamp'] + [s.Name + s.Identifier for s in sensors])
     
     while True:
+        hwmon = wmi.WMI(namespace="root\LibreHardwareMonitor")
+        sensors = hwmon.Sensor(SensorType="Temperature")
+
         try:
             # Write values, each column contains the value of the sensor
             writer.writerow([datetime.now()]+[s.Value for s in sensors])
